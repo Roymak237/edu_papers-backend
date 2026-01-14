32ecdeac6a1c4d550752f40fa0bbb32fdcfc74db
Kynmmarshall <kynmmarshall@gmail.com>
2026-01-14 18:49:02 +0100

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const db = require('./db');
+const db = require('./db/db');
 
 const app = express();
 
